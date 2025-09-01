4d4f267b54e4fd1aa4f9a31d83d8664611ebf68d
Abigail Ewnetu <abiz26@yahoo.com>
2025-09-01 17:08:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-//const passwordBox = document.getElementById('password');
-//const length = 12;
-
-//const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-//const lowerCase = "abcdefghijklmnopqrstuvwxyz";
-//const number = "0123456789";
-//const symbol = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
-//const allChars = upperCase + lowerCase + number + symobol;
-
-//function generatePassword() {
-    //let password = "";
-    //password += upperCase[Math.floor(Math.random() * upperCase.length)];
-    //password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
-    //password += number[Math.floor(Math.random() * number.length)];
-    //password += symbol[Math.floor(Math.random() * symbol.length)];
-
-    //while (password.length < length) {
-        //password += allChars[Math.floor(Math.random() * allChars.length)];
-    //}
-    //passwordBox.value = password;
-///}
-
-//function copyPassword() {
-    //passwordBox.select();
-    //document.execCommand('copy');
-//}
-
 
 const passwordBox = document.getElementById('password');
 const length = 12;
@@ -75,3 +48,4 @@ async function copyPassword() {
         alert("Failed to copy password.");
     }
 }
+
